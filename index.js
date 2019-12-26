5abdf0607d7aa369f068e86a5c3e4e1a287842ae
tranght <123hoangthitrang99@gmail.com>
2019-12-26 15:09:37 +0700

    update 26/12

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const bootstrap = async () => {
         await mongoose.connect('mongodb://admin:admin1@ds257698.mlab.com:57698/doan2019');
 
         // use middlewares + routers
-        // app.use(cors({
-        //     origin: ['http://localhost:3000', 'http://localhost:3001', 'https://fileupload-back-end.herokuapp.com', 'https://notarized.herokuapp.com'],
-        //     credentials: true,
-        // }));
-        app.use(cors())
+        app.use(cors({
+            origin: ['http://localhost:3000','http://localhost:3002', 'http://localhost:3001', 'https://fileupload-back-end.herokuapp.com', 'https://notarized.herokuapp.com'],
+            credentials: true,
+        }));
+        // app.use(cors())
         app.use(expressSession({
             secret: 'cobaudeptrai',
             resave: false,
